0bbccec78740380e6ac413d0d612325193c4934c
j143 <j143@protonmail.com>
2017-10-23 20:43:27 +0530

    Update GridSearchClassifierCV.java

--- src/main/java/org/apache/sysml/api/ml/GridSearchClassifierCV.java
+++ src/main/java/org/apache/sysml/api/ml/GridSearchClassifierCV.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -16,20 +16,24 @@
  * specific language governing permissions and limitations
  * under the License.
  */
- 
+
 package org.apache.sysml.api.ml;
 
+import org.apache.spark.ml.Estimator;
+
+import org.apache.sysml.api.ml.GridSearchClassifierCV;
+import org.apache.sysml.api.ml.GridSearchClassifierCVModel;
+
 public class GridSearchClassifierCV extends Estimator[GridSearchClassifierCVModel] {
-	
-	@abstract
-	public fit () {
-		
-	}
-	
-	@abstract
-	public transform () {
-		
-	}
-	
+
+        @abstract
+        public fit() {
+
+        }
+
+        @abstract
+        public transform() {
+
+        }
+
 }
- 
